0e5e9aaaf5556843fb504bc196c95ce2fd5b923e
hji <gkwldls2003@naver.com>
2024-10-29 17:57:39 +0900

    import 삭제

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import axios from "axios";
-import Script from "next/script"
-import { useEffect, useState } from "react";
 import { NavermapsProvider  } from 'react-naver-maps'
-import $ from "jquery";
 import dynamic from 'next/dynamic';
 
 //react-naver-maps/dist/chunk-SENI7KNJ.mjs SSR 환경에서 임포트하면 오류나서 false 처리
