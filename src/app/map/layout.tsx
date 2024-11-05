3b7a9a3d9ba1e25b4c54154c24af183e88896d18
hji <gkwldls2003@naver.com>
2024-11-05 15:26:06 +0900

    use client 삭제

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react';
 import Header from '@/layout/header/page';
 import Footer from '@/layout/footer/page';
--- src/layout/sider/page.tsx
+++ src/layout/sider/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react';
 import { useAppSelector } from '../../../store/hooks';
 import { selectVisibleMarkers } from '../../../store/markerSlice';
