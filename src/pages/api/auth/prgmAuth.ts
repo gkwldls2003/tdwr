dd44e6d3de81d2de937b64db79fa8c4aa80ed82d
hji <gkwldls2003@naver.com>
2024-11-23 09:03:25 +0900

    prgmAuth, reqUrl 경로 변경

---
+++ src/pages/api/auth/prgmAuth.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { selectUserAuthPrgmQuery } from '../../../../common/querys/auth/page';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
+    const { author_id } = JSON.parse(req.body);
+    const prgmList = await selectUserAuthPrgmQuery([author_id]);
+    return res.status(200).json(prgmList.data);
+  } catch (error) {
+    console.error('Error in auth check:', error);
+    return res.status(500).json({ message: '500 error' });
+  }
+}
