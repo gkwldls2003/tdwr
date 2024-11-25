dd681d9bd106b34d27855c620b3e93a370ca589b
hji <gkwldls2003@naver.com>
2024-11-25 16:02:47 +0900

    파일 유효성 검사 client단으로 수정

--- common/utils/cmm/cmm.ts
+++ common/utils/cmm/cmm.ts
@@ -1,6 +1,6 @@
 import { inserFileDataQuery } from "../../querys/cmm/page";
 
-export default async function fileUpload(file:File | null, id:number, table:string, colunm_nm:string, sn?: number) {
+export async function fileUpload(file:File | null, id:number, table:string, colunm_nm:string, sn?: number) {
 
     if (!file) return;
 
@@ -49,4 +49,34 @@ export default async function fileUpload(file:File | null, id:number, table:stri
       alert(error);
     } 
  
+}
+
+export function isFileValidate(file:File | null) {
+  let isValid = false;
+  const MAX_FILE_SIZE = 2 * 1024 * 1024; //파일 최대 크기 2메가
+  const ALLOWED_FILE_TYPES = ['jpg','jpeg','png','gif','bmp','pdf','doc','docx','xls','xlsx','ppt','pptx','hwp','hwpx','txt','zip'];
+
+  if (!file) {
+    isValid = true;
+    alert("파일이 없습니다.");
+
+    return isValid;
+  }
+
+  if (file.size > MAX_FILE_SIZE) {
+    
+    isValid = true;
+    alert(`파일 크기는 ${MAX_FILE_SIZE / (1024 * 1024)}MB를 초과할 수 없습니다.`);
+
+    return isValid;
+  }
+
+  const fileExt = file.name.split('.')[1];
+  if (!ALLOWED_FILE_TYPES.includes(fileExt)) {
+    isValid = true;
+    alert("지원하지 않는 파일 형식입니다.");
+
+    return isValid;
+  }
+
 }
