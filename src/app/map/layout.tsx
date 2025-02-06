3b71ce04e42e08fae4342111a1e45522f2843656
hji <gkwldls2003@naver.com>
2025-02-06 14:08:03 +0900

    Map tailwind에서 mui로 수정, layout폴더에 있는 파일들 Header와 Footer제외하고 삭제

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -1,31 +1,13 @@
 'use client'
 
 import React from 'react';
-import LeftSider from '@/layout/sider/page';
 import { Provider } from 'react-redux';
 import { store } from "../../../store/store";
-import { useSelector } from 'react-redux';
-import { selectShowLeftSider } from '../../../store/layoutSlice';
-
-function LayoutContent({ children }: { children: React.ReactNode }) {
-  const showLeftSider = useSelector(selectShowLeftSider);
-
-  return (
-    <div className="flex flex-col h-screen w-full">     
-      <div className="flex flex-1 overflow-hidden">
-        {showLeftSider && <LeftSider/>}
-        <div className="flex-1 relative">
-          {children}
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <LayoutContent>{children}</LayoutContent>
+      {children}
     </Provider>
   );
 }
